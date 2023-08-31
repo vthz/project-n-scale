42245926caeb4ade3d91333986093ef3a4e95fe6
Shaheen Naiyer <snshohin@gmail.com>
2023-08-31 15:13:16 +0530

    Added basic python serial code

---
+++ Python/CommandsFile.py
@@ -0,0 +1,26 @@
+project1 = "Intermodal" + " "
+project2 = "Coal Unloading" + " "
+project3 = "Coal Loading" + " "
+
+command_list_v1 = [  # interactive command
+    ["100", project1 + "Reset position"],
+    ["101", project1 + "Move Forward"],
+    ["102", project1 + "Move Backward"],
+    ["103", project1 + "Move Upward"],
+    ["104", project1 + "Move Downward"],
+    ["105", project1 + "Move Left"],
+    ["106", project1 + "Move Right"],
+
+    ["200", project2 + "Reset Position"],
+    ["201", project2 + "Rotate 180 CW"],
+
+    ["300", project3 + "Reset Loading Gate"],
+    ["301", project3 + "Toggle Loading Gate"],
+]
+
+command_list_v2 = [   # freestyle commands
+    ["W", "Move Forward"],
+    ["S", "Move Backward"],
+    ["A", "Move Left"],
+    ["D", "Move Right"]
+]
---
+++ Python/SerialCom.py
@@ -0,0 +1,43 @@
+import keyboard
+import serial
+import time
+from CommandsFile import command_list_v2 as command_list
+
+COM_PORT = "COM3"
+
+SERIAL = serial.Serial(COM_PORT, baudrate=9600, timeout=1)
+COMMAND_SET = set(item[0] for item in command_list)
+time.sleep(0)  # Wait for 3 seconds
+
+loop_flag = True
+freestyle_mode = True
+
+while loop_flag and not freestyle_mode:
+    user_command = input("=->")
+
+    if user_command == "X":
+        loop_flag = False
+        break
+    elif user_command.lower() == "help":
+        for single_command in command_list:
+            print(single_command)
+    elif user_command is not None and user_command in COMMAND_SET:
+        print("=->" + user_command)
+        SERIAL.write(user_command.encode())
+        arduino_response = SERIAL.readline().decode().split("\r\n")
+        print(arduino_response)
+    else:
+        print("Invalid Command!")
+
+while loop_flag and freestyle_mode:
+    key = keyboard.read_key()
+    if key:
+        print(key)
+        if key.upper() == "X":
+            loop_flag = False
+            break
+        elif key.upper() in COMMAND_SET:
+            SERIAL.write(key.encode)
+            pass
+        else:
+            print("Invalid Key")
