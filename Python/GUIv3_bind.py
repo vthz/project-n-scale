8ec9be36f3b01bedb4c3e66874ecb9197f393864
Shaheen Naiyer <snshohin@gmail.com>
2023-09-12 17:06:44 +0530

    Added Toggle Switches to GUI

--- Python/GUIv3_bind.py
+++ Python/GUIv3_bind.py
@@ -4,6 +4,10 @@ import customtkinter as ct
 class App(ct.CTk):
     def __init__(self):
         super().__init__()
+        self.switch1_flag = False
+        self.switch2_flag = False
+        self.switch3_flag = False
+
         self.geometry("220x360")
         self.counter_var = 0
         self.title("Python - Arduino GUI with Key Binding v3")
@@ -19,6 +23,13 @@ class App(ct.CTk):
 
         self.logs_label = ct.CTkLabel(self, text_color="black", text="Welcome!!!")
         self.logs_label.pack(anchor="center")
+
+        self.switch_1 = ct.CTkSwitch(self, text="LED 1", onvalue=True, offvalue=False)
+        self.switch_1.pack(anchor="center")
+        self.switch_2 = ct.CTkSwitch(self, text="LED 2", onvalue=True, offvalue=False)
+        self.switch_2.pack(anchor="center")
+        self.switch_3 = ct.CTkSwitch(self, text="LED 3", onvalue=True, offvalue=False)
+        self.switch_3.pack(anchor="center")
 
         self.speed_slider = ct.CTkSlider(self, from_=0, to=100, number_of_steps=100, width=200)
         self.speed_slider.set(0)
@@ -38,12 +49,27 @@ class App(ct.CTk):
         elif variable == "Pressed_M":
             final_var_to_serial = "M" + ":" + str("{:.2f}".format(self.speed_slider.get()))
         elif variable == "Pressed_1":
+            self.switch1_flag = not self.switch1_flag
+            if self.switch1_flag:
+                self.switch_1.select()
+            else:
+                self.switch_1.deselect()
             final_var_to_serial = "LED:1"
         elif variable == "Pressed_2":
+            self.switch2_flag = not self.switch2_flag
+            if self.switch2_flag:
+                self.switch_2.select()
+            else:
+                self.switch_2.deselect()
             final_var_to_serial = "LED:2"
         elif variable == "Pressed_3":
+            self.switch3_flag = not self.switch3_flag
+            if self.switch3_flag:
+                self.switch_3.select()
+            else:
+                self.switch_3.deselect()
             final_var_to_serial = "LED:3"
-        self.logs_label.configure(text="~> " + final_var_to_serial)
+        self.logs_label.configure(text="-=> " + final_var_to_serial)
         print(final_var_to_serial)
 
 
